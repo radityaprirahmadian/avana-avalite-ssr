2fa69244886c5ed07233199907234af24e5c0ee5
ajips <aji@avana.id>
2021-10-06 11:16:36 +0700

    add async

--- src/helpers/loadScript.js
+++ src/helpers/loadScript.js
@@ -1,14 +1,13 @@
 export default function loadScript(src) {
   return new Promise(function (resolve, reject) {
+    var s = document.createElement('script');
+    s.async = true;
     if (typeof src === 'function') {
-      var s = document.createElement('script');
       // s.src = chrome.runtime.getURL('script.js');
       s.textContent = src();
       (document.head || document.documentElement).appendChild(s);
       return;
     }
-    var s;
-    s = document.createElement('script');
     s.src = src;
     s.onload = resolve;
     s.onerror = reject;
