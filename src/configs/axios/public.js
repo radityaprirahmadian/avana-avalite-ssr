a8304524c5747394041f9c7bda5579710eef64d4
ajips <aji@avana.id>
2021-10-13 14:00:45 +0700

    add axios header prevent disk cache

--- src/configs/axios/public.js
+++ src/configs/axios/public.js
@@ -4,6 +4,11 @@ import errorHandler from './errorHandler'
 
 const instance = axios.create({
    baseURL: `${process.env.NEXT_PUBLIC_API_HOST}/api/avalite`,
+   headers: {
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+   }
 })
 
 instance.interceptors.response.use((response) => response.data, errorHandler)
