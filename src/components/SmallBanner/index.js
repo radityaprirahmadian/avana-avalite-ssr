2a3428902f5f3dd5e919fcf4d8c83f03a23028c3
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-06-17 11:19:58 +0700

    consume and implement image props drilling for small banner

--- src/components/SmallBanner/index.js
+++ src/components/SmallBanner/index.js
@@ -1,18 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import Img from '../Container/Image'
 
-const SmallBanner = () => {
-  // Imitate API call
-  const [bannerList, setBannerList] = useState([])
-  useEffect(() => {
-    setBannerList([1, 2, 3, 4, 5, 6, 7, 8])
-  }, [])
+const SmallBanner = (props) => {
+
+  // Get only the small banner image
+  const imgList = useMemo(() => {
+    const smallBannerFilter = (imageObject) => imageObject.type === 1;
+
+    return (
+       (props?.imgKey
+          ? props?.images?.filter(smallBannerFilter).map((data) => data?.[props?.imgKey] || '')
+          : props?.images) || []
+    )
+ }, [props?.images])
   
   return (
     <section className="grid grid-cols-4">
-      {bannerList.map((data, index) => (
+      {imgList.map((image, index) => (
         <div key={index} className="relative w-full border border-transparent">
-          <h2 style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} className="absolute z-10 text-white text-3xl">{data}</h2>
-          <img src={`https://picsum.photos/id/${data}/200/200`} className="h-full w-full opacity-75" />
+          <h2 style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} className="absolute z-10 text-white text-xl">Overlay</h2>
+          <Img src={image.replace('thumbnail', 'large')}className="h-full w-full opacity-75" />
         </div>
       ))}
     </section>
